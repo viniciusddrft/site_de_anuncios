68a19738aabab66b721e9280c6e3a23ecc333f74
viniciusddrft <viniciusddrft5@gmail.com>
2020-02-23 23:25:18 -0300

    anuncio.html criado

--- anuncios/urls.py
+++ anuncios/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from .views import home
 from .views import categoria
-
+from .views import anuncio
 
 urlpatterns = [
     path('', home, name='home'),
-    path('<int:categoria_id>', categoria, name='categoria')
+    path('categoria<int:categoria_id>', categoria, name='categoria'),
+    path('anuncio<int:anuncio_id>', anuncio, name='anuncio'),
 ]
